86d080ae3ceca95a06a8a64ec4550d5175d1cdd7
Tim Gavin <tim@timgavin.name>
2014-10-07 10:35:23 -0700

    Edit text

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,5 @@
 <?php
-	// check if user is is authorized with Tumblr
+	// check if user is authorized with Tumblr
 	function check_tumblr_oauth_access($db) {
 		if($oauth = $db->get_row("SELECT oauth_username,oauth_token,oauth_secret FROM users_oauth WHERE uo_usr_id = ".$db->escape($_SESSION['usr']['id'])." AND oauth_provider = 'tumblr'")) {
 			$tumblroauth = new TumblrOAuth(TUMBLR_OAUTH_KEY, TUMBLR_OAUTH_SECRET, $oauth->oauth_token, $oauth->oauth_secret);
